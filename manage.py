95803ea54ab7b6c204cd7291bc7fed6492be9a0f
Lance Liang <LanceLiang2018@163.com>
2019-11-16 20:21:21 +0800

    美化(?)一下页面

--- manage.py
+++ manage.py
@@ -10,8 +10,9 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    return '<a href="https://github.com/LanceLiang2018/Wenku8ToEpub-Online">' \
-           'https://github.com/LanceLiang2018/Wenku8ToEpub-Online</a>'
+    # return '<a href="https://github.com/LanceLiang2018/Wenku8ToEpub-Online">' \
+    #        'https://github.com/LanceLiang2018/Wenku8ToEpub-Online</a>'
+    return render_template('index.html')
 
 
 @app.route('/cache/<int:book_id>')
@@ -45,6 +46,36 @@ def get(book_id: int):
         return '没有这个小说！'
     filename = "%s.epub" % filename_
     return redirect('https://light-novel-1254016670.cos.ap-guangzhou.myqcloud.com/%s' % filename)
+
+
+@app.route('/name/<string:book_name>')
+def jump_by_name(book_name: str):
+    filename = "%s.epub" % book_name
+    return redirect('https://light-novel-1254016670.cos.ap-guangzhou.myqcloud.com/%s' % filename)
+
+
+@app.route('/search')
+def search():
+    args0 = dict(request.args)
+    args = {}
+    for arg in args0:
+        v = args0[arg]
+        if type(v) is list:
+            args[arg] = v[0]
+        else:
+            args[arg] = v
+    print(args)
+    method = args['method']
+    search_key = args['search_key']
+    bid = None
+    if method == 'id':
+        try:
+            bid = int(search_key)
+        except ValueError:
+            return "ID输入错误！"
+        return redirect('/get/%s' % bid)
+    else:
+        return redirect('/name/%s' % search_key)
 
 
 if __name__ == '__main__':
